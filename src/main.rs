3ff879d680d4e8c5490ee5cf476fd35f9a223dea
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2024-06-24 16:58:48 +0200

    Make it a little bit more robust
    - Don't let a user start two instances of the same project.
    - Explicitly call cleanup state instead of it being a side effect of getting running projects

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,23 @@ lazy_static! {
         .expect("Couldn't find config dir");
 }
 
+fn try_cleanup_state() -> Result<(), anyhow::Error> {
+    let running_projects = get_running_projects()?;
+    for p in running_projects.iter() {
+        let _ = std::fs::remove_file(LOG_DIR.join(&p.name));
+    }
+
+    for entry in std::fs::read_dir(STATE_DIR.as_path())? {
+        let path = entry?.path();
+        let (_, sid) = parse_state_filename(&path)?;
+        if !has_processes_running(sid) {
+            let _ = std::fs::remove_file(path);
+        }
+    }
+
+    Ok(())
+}
+
 // Try to get vec of running projects. Try to remove the state file if the process is not running
 fn get_running_projects() -> Result<Vec<MinimalProject>, anyhow::Error> {
     let projects = std::fs::read_dir(STATE_DIR.as_path())?
@@ -38,17 +55,12 @@ fn get_running_projects() -> Result<Vec<MinimalProject>, anyhow::Error> {
             let path = entry.ok()?.path();
             let (name, sid) = parse_state_filename(&path).ok()?;
             let project = Project::from_str(&name).ok()?;
-            has_processes_running(sid)
-                .then_some(MinimalProject {
-                    name,
-                    display: project.display,
-                    stop_signal: project.stop_signal,
-                    pid: sid,
-                })
-                .or_else(|| {
-                    std::fs::remove_file(path).ok()?;
-                    None
-                })
+            has_processes_running(sid).then_some(MinimalProject {
+                name,
+                display: project.display,
+                stop_signal: project.stop_signal,
+                pid: sid,
+            })
         })
         .collect::<Vec<_>>();
 
@@ -118,14 +130,21 @@ fn status() -> Result<(), anyhow::Error> {
 fn stop(projects: Vec<Project>) -> Result<(), anyhow::Error> {
     let running_projects = get_running_projects()?;
 
-    // Be kind and use `SIGINT` for the first time we try to stop the projects.
-    for project in projects.iter() {
-        if let Some(p) = running_projects.iter().find(|p| p.name == project.name) {
-            let signal = p.stop_signal.as_ref().unwrap_or(&Signal::SIGINT);
-            let _ = stop_pg(p.pid, signal);
-        } else {
-            eprintln!("Cannot stop project not running: {}", project.name);
-        }
+    let (running, not_running): (Vec<_>, Vec<_>) = projects
+        .into_iter()
+        .partition(|rp| running_projects.iter().any(|p| p.name == rp.name));
+
+    for project in running.iter() {
+        let p = running_projects
+            .iter()
+            .find(|it| it.name == project.name)
+            .unwrap();
+        let signal = project.stop_signal.as_ref().unwrap_or(&Signal::SIGINT);
+        let _ = stop_pg(p.pid, signal);
+    }
+
+    for project in not_running {
+        eprintln!("Cannot stop project not running: {}", project.name);
     }
 
     let timeout = Duration::new(5, 0);
@@ -138,13 +157,17 @@ fn stop(projects: Vec<Project>) -> Result<(), anyhow::Error> {
         // them
         running_projects = get_running_projects()?
             .into_iter()
-            .filter(|rp| projects.iter().any(|p| rp.name == p.name))
+            .filter(|rp| running.iter().any(|p| rp.name == p.name))
             .collect();
+
+        try_cleanup_state()?;
 
         if running_projects.is_empty() {
             return Ok(());
         }
     }
+
+    try_cleanup_state()?;
 
     // If neither the kind `SIGINT` or forceful `SIGKILL` didn't work, we need to print out that it
     // failed to stop the projects...
@@ -159,8 +182,20 @@ fn stop(projects: Vec<Project>) -> Result<(), anyhow::Error> {
 }
 
 fn start(projects: Vec<Project>) -> Result<(), anyhow::Error> {
+    let running_projects = get_running_projects()?;
+    let (running, not_running): (Vec<_>, Vec<_>) = projects
+        .into_iter()
+        .partition(|p| running_projects.iter().any(|rp| rp.name == p.name));
+
+    for project in running {
+        eprintln!(
+            "{} is already running",
+            project.display.unwrap_or(project.name)
+        );
+    }
+
     let master_pid = sysinfo::get_current_pid().unwrap();
-    for project in projects {
+    for project in not_running {
         if let Fork::Child =
             daemon(&project).map_err(|e| anyhow!("Error: {} on daemon: {:?}", e, project))?
         {
